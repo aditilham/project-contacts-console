b957a5864b4888d6d24c3045d332e89aa7452d7a
aditilham <aditilham152@gmail.com>
2019-06-26 15:16:44 +0700

    first commit

---
+++ contactsData.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const contacts = [{
+        name: "Andi",
+        phone: "+1 111 101010",
+        email: "alpha@avalon.org"
+    },
+    {
+        name: "Budi",
+        phone: "+62 812 242424",
+        email: "betty@bahamut.com"
+    },
+    {
+        name: "Citra",
+        phone: "+63 813 003636",
+        email: "cello@charizard.dev"
+    },
+    {
+        name: "Desi",
+        phone: "+1 111 101230",
+        email: "draco@draconian.org"
+    },
+    {
+        name: "Ehsan",
+        phone: "+62 812 849358",
+        email: "ender@eager.com"
+    },
+    {
+        name: "Farhan",
+        phone: "+63 813 048175",
+        email: "fantastic@fang.dev"
+    }
+];
---
+++ index.js
@@ -0,0 +1,65 @@
+"use strict"
+
+let showContacts = () => {
+    for (let person of contacts) {
+        let contact = "";
+        contact += (`${person.name} `)
+        contact += (`(${person.phone}) `)
+        contact += (`<${person.email}>`)
+
+        console.log(contact)
+        alert(contact)
+    }
+}
+
+let searchContacts = (personName) => {
+    let contact = "";
+    let flag = 0;
+    for (let person of contacts) {
+        if (person.name.toLowerCase() == personName.toLowerCase()) {
+            contact += (`${person.name} `)
+            contact += (`(${person.phone}) `)
+            contact += (`<${person.email}>`)
+            flag = 1;
+        }
+        continue;
+    }
+    console.log((flag == 1) ? contact : "No contact found with that name")
+    alert((flag == 1) ? contact : "No contact found with that name")
+}
+
+let addContact = (newName, newPhone, newEmail) => {
+    let newContact = {
+        name: newName,
+        phone: newPhone,
+        email: newEmail
+    }
+    contacts.push(newContact)
+}
+
+let menu = 0;
+
+while (menu == 0) {
+    let menuInput = parseInt(prompt('What do you want to? 1.Show Contact 2.Search Contact 3.Input New Contact 4.Exit'))
+    switch (menuInput) {
+        case 1:
+            showContacts();
+            break;
+        case 2:
+            let searchName = prompt('Who are you looking for?')
+            searchContacts(searchName);
+            break;
+        case 3:
+            let addName = prompt('Input new contact name')
+            let addNumber = prompt('Input new contact number')
+            let addEmail = prompt('Input new contact email')
+            addContact(addName, addNumber, addEmail)
+            break;
+        case 4:
+            menu = 1;
+            break;
+        default:
+            break;
+    }
+}
+alert('Thanks for using Contact console, all data that you already input will be automatically deleted.')
